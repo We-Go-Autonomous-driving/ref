805d0d5a629d8fb6b12b6d49fefe986eabe769f3
Shinest-changwon <changwon1025@gmail.com>
2021-06-17 00:05:14 +0900

    장애물 급정지 코드 추가 by 김강태

--- module/utils2.py
+++ module/utils2.py
@@ -3,13 +3,16 @@ import numpy as np
 
 def person_dist(depth_frame, cx, cy, h):
     box_center_roi = np.array((depth_frame[cy-(h//5)-10:cy-(h//5)+10, cx-10:cx+10]),dtype=np.float64)
+    box_center_roi = np.where(box_center_roi<1, np.mean(box_center_roi), box_center_roi)
     mean_dist = cv2.mean(box_center_roi)
 
     return mean_dist[0]
 
 def obstacle_detect(default, depth_frame, offset = 100):
-    key1 = ''
-    key2 = ''
+
+    sudden_roi = np.array((depth_frame[375:385, 315:325]),dtype=np.float64)
+    sudden_roi = np.where(sudden_roi<1, np.mean(sudden_roi), sudden_roi)
+    mean = np.mean(sudden_roi)
 
     # obstacle detection roi 10x10
     obs_roi1 = np.array((depth_frame[435:445, 195:205]),dtype=np.float64)
@@ -39,21 +42,16 @@ def obstacle_detect(default, depth_frame, offset = 100):
 
     #print('default distance : ', roi1_min, roi2_min, roi3_min, roi4_min)
 
+    if mean < 1500 :
+        return 'stop'
+
     if mean1<roi1_min :
-        #print('turn right faster')
-        key1 = 'turn_right'
-        return key1
+        return 'right'
     elif mean2<roi2_min : 
-        #print('turn right')
-        key1 = 'trun_right'
-        return key1
+        return 'right'
 
     if mean4<roi4_min :
-        #print('turn left faster')
-        key2 = 'turn_left'
-        return key2
+        return 'left'
     elif mean3<roi3_min :
-        #print('turn left')
-        key2 = 'trun_left'
-        return key2
+        return 'left'
     
